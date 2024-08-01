319c124a943babd2eaa1be11d1d0adbec2949ae4
Shin <160511537+glacierrr1@users.noreply.github.com>
2024-08-01 11:55:03 +0800

    manager.py

--- manager.py
+++ manager.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 import json
 
 class manager:
@@ -6,14 +5,24 @@ class manager:
         for i in range(len(ck)):
             tk , mk = ck[i].split('|')
             tree.insert("", "end", values=(acc+i, tk,mk,))
+
+    def mail(self,mail,tree,somail):
+        for i in range(len(mail)):
+            tk , mk = mail[i].split('|')
+            tree.insert("", "end", values=(somail+i, tk,mk,))
         
-    def setting(self,width,height,soclone,luong,delay):
+    def setting(self,width,height,soclone,luong,delay,thaymail,doipass,logclonep,logclone,doitt):
         config_data = {
             'width': width,
             'height': height,
             'soclone': soclone,
             'luong': luong,
             'delay': delay,
+            'thaymail': thaymail,
+            'doipass': doipass,
+            'logclonep': logclonep,
+            'logclone': logclone,
+            'doitt': doitt,
         }
 
         with open('Advanced_settings.json', 'w',encoding='utf-8') as configfile:
